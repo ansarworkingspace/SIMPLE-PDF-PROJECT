966155766aa632df4fb60256cd720d76296c0631
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-10-24 20:48:21 +0530

    Started the 'SimplePDF' project, using MERN stack technology and various other trending technologies.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
