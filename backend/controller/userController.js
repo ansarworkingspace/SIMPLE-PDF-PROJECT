01cffddc0a23931ff2330b7468b782f56886c67c
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-10-25 20:21:03 +0530

    After completing the basic setup of the backend, I moved on to creating the frontend and cleaning up the frontend code. Additionally, I set up the backend and frontend ports to run concurrently.

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
+import User from '../models/userModels.js';
 import generateToken from '../utils/generateToken.js';
 
 
